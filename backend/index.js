5f13b64b2bf6114021dc106b9ceec3c1970f7fbc
Frostyrr <marcelson.rebato@gmail.com>
2024-12-30 19:13:00 +0800

    project -- connectedDB, routes, and models (ongoing)

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js"
+
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -6,6 +12,9 @@ app.get('/', (req, res) => {
     res.send("Hello world 123!")
 })
 
+app.use("/api/auth", authRoutes)
+
 app.listen(3000, () => {
+    connectDB();
     console.log("Server started at 3000")
 })
